1b9fb719b980d32e9197388664bb8ec77fe830e0
Akash K <akash178kusha@gmail.com>
2025-07-08 12:38:11 +0530

    Added Pattern4.java and Array1.java

---
+++ Array1.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of students: ");
+        int n = sc.nextInt();
+        int arr[] = new int [n];
+      
+
+        for(int i =0; i<n-1;i++){
+              System.out.println("Enter the marks of the student no : " + (i+1));
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The scores are : ");
+        for(int i = 0; i<=n-1; i++){
+            System.out.println("The marks of the students " + (i+1) + " is " + arr[i]);
+        }
+    }
+}
